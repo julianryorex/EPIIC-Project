f669449a21cc1eeb8b8ca49e9507f583616f2d95
Julian Rex <julian.rechsteiner@gmail.com>
2020-02-06 19:07:20 -0700

    top navbar is finally working

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,22 +3,45 @@ import React from 'react';
 
 function Header() {
     return (
-      <div>
-            <Banner />
-            <div className="row">
-                <div className="col justify-content-md-center">hello</div>
-                <div className="col justify-content-md-center">hello</div>
-                <div className="col justify-content-md-center">hello</div>
-            </div>
-      </div>
+    	<div>
+    		<Banner />   
+			</div>
     );
 }
 
 
 function Banner() {
-    return(
-        <div className="banner"></div>
-    )
+    return (
+      <nav className="banner navbar-expand-lg navbar navbar-dark bg-dark sticky-top">
+        <a className="navbar-brand mb-0 h1" href="#">
+          EPIIC
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link active" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-item nav-link" href="#">
+              About
+            </a>
+            <a className="nav-item nav-link" href="#">
+              Contact
+            </a>
+          </div>
+        </div>
+      </nav>
+    );
 }
 
 export default Header;
