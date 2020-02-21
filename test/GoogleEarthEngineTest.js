6155ab65b3658827081fb8c0998e45d4abb45f39
Gibbedboy <31701378+Gibbedboy@users.noreply.github.com>
2020-02-21 08:41:27 -0700

    Earth Engine Tests
    More GEE (Google Earth Engine) wackiness

--- test/GoogleEarthEngineTest.js
+++ test/GoogleEarthEngineTest.js
@@ -5,32 +5,51 @@ Not sure how to get running on REACT yet.
 */
 var ee = require('@google/earthengine');
 
-// GPM V6 on 2020-1-14, in Bozeman for that day
+// GPM V6 on 2020-1-01, in Bozeman
 var bozemanPoint = ee.Geometry.Point(111, 45);
 
-var range = ee.Date('2020-1-14').getRange('day');
-var dataset = ee.ImageCollection('NASA/GPM_L3/IMERG_V06')
-    .filter(ee.Filter.date(range));
-print("dataset: ", dataset);
-// Select the max preciptation and mask out low precipitation values.
-var precipitation = dataset.select('precipitationCal').max();
-var mask = precipitation.gt(0.3);
-var precipitation = precipitation.updateMask(mask);
+// Init 1st image composite (1st Week of Jan, 2019)
+var week1 = ee.Date('2019-01-01').getRange('day');
+var dataset1 = ee.ImageCollection('NASA/GPM_L3/IMERG_V06')
+    .filterBounds(ee.Geometry.Point(111, 45))
+    .filter(ee.Filter.date(week1));
+    
+// Init 2nd image composite (2nd Week of Jan, 2020)
+var week2 = ee.Date('2020-01-01').getRange('day');
+var dataset2 = ee.ImageCollection('NASA/GPM_L3/IMERG_V06')
+    .filterBounds(ee.Geometry.Point(111, 45))
+    .filter(ee.Filter.date(week2));
 
-var palette = [
-  '000096','0064ff', '00b4ff', '33db80', '9beb4a',
-  'ffeb00', 'ffb300', 'ff6400', 'eb1e00', 'af0000'
-];
-var precipitationVis = {min: 0.0, max: 5.0, palette: palette, opacity: .75};
-Map.addLayer(precipitation, precipitationVis, 'Precipitation');
-Map.setCenter(-111, 45, 9);
+// Select the max preciptation
+var precip1 = dataset1.select('precipitationCal');
+var precip2 = dataset2.select('precipitationCal');
+//var mask = precipitation.gt(0.0);
+//var precipitation = precipitation.updateMask(mask);
 
-// Export geoTIFF
+// This doesn't actually work, need to reduce the image collection into a single image first.
+var precipDiff = precip1.subtract(precip2);
+
+// Init Visualization Styling
 /*
-Export.image.toDrive({
-  image: precipitation,
-  description: 'test',
-  dimensions: 4000,
-  region: gallatinValley
-});
+var palette = [
+  'DarkBlue','MediumBlue', 'Blue', 'ForestGreen', 'SpringGreen',
+  'Yellow', 'Orange', 'OrangeRed', 'Red', 'DarkRed'
+];
 */
+var palette = [
+  'DarkRed', 'Green', 'Dark Blue'
+];
+
+var precipitationVis = {
+  min: -10, 
+  max: 20, 
+  palette: palette, 
+  opacity: .75};
+
+// Add layer to map
+Map.addLayer({
+  eeObject: precipDiff,
+  name: "Precipitation Difference",
+  visParams: precipitationVis
+});
+Map.setCenter(-111, 45, 6);
