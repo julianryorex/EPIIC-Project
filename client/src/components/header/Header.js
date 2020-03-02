ecb9e126c5540ced0ab7ec984cbdfe53a36c0c0b
Julian Rex <julian.rechsteiner@gmail.com>
2020-03-01 19:17:30 -0700

    fixed href glitch in header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -15,36 +15,38 @@ function Header() {
 
 function Banner() {
     return (
-      <nav className="banner navbar-expand-md navbar navbar-dark bg-dark sticky-top">
-        <li>
-          <NavLink className="navbar-brand mb-0 h1" to="/Main">
-            EPIIC Project
-          </NavLink>
-          <span className="sr-only">(current)</span>
-        </li>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <li>
-              <Link to="/About">About</Link>>
-            </li>
-            <li>
-              <Link to="/Contact">Contact</Link>
-            </li>
-          </div>
-        </div>
-      </nav>
-    );
+		<nav className="banner navbar-expand-md navbar navbar-dark bg-dark sticky-top">
+			<li>
+				<NavLink className="navbar-brand mb-0 h1" to="/Main">
+					EPIIC Project
+				</NavLink>
+				<span className="sr-only">(current)</span>
+			</li>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div className="navbar-nav">
+					<li>
+						<NavLink className="nav-item nav-link" to="/About">
+							About
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className="nav-item nav-link" to="/Contact">Contact</NavLink>
+					</li>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default Header;
