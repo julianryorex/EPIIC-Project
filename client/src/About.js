b77a90382e98343a6932c32fafc6e857530e9cbf
Julian Rex <julian.rechsteiner@gmail.com>
2020-03-06 15:47:48 -0700

    added link to repo

--- client/src/About.js
+++ client/src/About.js
@@ -5,6 +5,7 @@ function About(){
         <div>
             <h1>About Page</h1>
             <p>Under Construction</p>
+            <h6>See our <a href="https://github.com/julianryorex/EPIIC-Project">Github Repository</a> for this project.</h6>
         </div>
     )
 }
