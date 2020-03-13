542c95a7d725d71b290b4cfd7a6c12bdf7efd4d9
Julian Rex <julian.rechsteiner@gmail.com>
2020-03-13 13:14:50 -0600

    final commit before deployment

--- app.js
+++ app.js
@@ -16,16 +16,16 @@ app.use(express.json());
 app.use('/api', routes);
 
 
-// // serve static assets if in production
-// if(process.env.NODE_ENV === 'production') {
-//     // set static folder
-//     app.use(express.static('client/build'));
+// serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
 
-//     app.get('*', (req, res) => {
+    app.get('*', (req, res) => {
 
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 app.listen(PORT, () => {
