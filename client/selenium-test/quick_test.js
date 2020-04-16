e12ed759ec37d8fdf0ba94e197d8150648651532
Kala <michaela_lightner@yahoo.com>
2020-04-16 13:23:46 -0600

    homepage selenium test

--- client/selenium-test/quick_test.js
+++ client/selenium-test/quick_test.js
@@ -13,20 +13,32 @@ driver.manage().window().maximize();
     await driver.manage().setTimeouts( { implicit: 10000 } );
     await driver.get('https://epiic-center.herokuapp.com/');
 
-//entering Start Date
+    //entering Start Date
     const sD = await driver.findElement(By.id('startDate'));
     sD.sendKeys('05/11/2015');
 
-//entering End Date
+    //entering End Date
     const eD = await driver.findElement(By.id('endDate'));
     eD.sendKeys('08/17/2018');
 
-//entering Top Left Corner
-//driver.findElement(By.id('map')).click();
+    //entering Top Left Corner marker
     const map = await driver.findElement(By.id('map'));
     map.click();
-//map.sendKeys('plus'); 
-//driver.moveToElement(map, 20, 25).perform(); 
-//map.sendKeys(webdriver.Key.chord(webdriver.Key.CONTROL, webdriver.Key.ADD));
 
+    //these next two lines create the second marker, although they don't make sense
+    //changing anything but the numbers will make the second marker not appear
+    //Having issues moving the marker to a different spot than the first
+    const map2 = await driver.findElement(By.id('map'), 57, 23).click();
+    map.click(88, 100);
+
+    
+    const submitButton = await driver.findElement(By.css('[type=submit]'));
+    submitButton.click();
+
+    let alert = (await driver).switchTo().alert(); 
+    alert.accept(); 
+    
+    (await driver).sleep(10000).then(function(){
+        driver.quit(); 
+    });
 }());
